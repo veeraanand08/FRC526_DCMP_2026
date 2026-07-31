ca2a20f5638fb252ef7f183ea5e6f136bfdcbc8a
alawn <leckiqie@gmail.com>
2026-07-31 17:05:23 -0400

    BAD IDEA

--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -133,7 +133,7 @@ public class Intake extends ExtendedSubsystem {
     if (pivotState != PivotState.LOWERING) {
       setPivotState(PivotState.LOWERING);
     } else if (pivot.getPositionDeg() > 100.0) {
-      pivot.runOpenLoop(1.0);
+      pivot.runOpenLoop(0.2);
     }
     roller.runClosedLoop(IntakeConstants.ROLLER_RPS);
     LED.getInstance().intake();
